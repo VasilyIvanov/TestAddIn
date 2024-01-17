ac3b2e5255edc57874b6113e1e0165c7692f5347
VasilyIvanov <59409548+VasilyIvanov@users.noreply.github.com>
2024-01-17 11:51:04 +1100

    Update taskpane.ts

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -9,21 +9,55 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("run").onclick = runMulti;
+
+    for (let i = 1; i <= 15; i++) {
+      console.log(`Req set 1.${i}`, Office.context.requirements.isSetSupported("Mailbox", `1.${i}`));
+    }
 
     // Register an event handler to identify when messages are selected.
-    Office.context.mailbox.addHandlerAsync(Office.EventType.SelectedItemsChanged, run, asyncResult => {
+    Office.context.mailbox.addHandlerAsync(Office.EventType.ItemChanged, runSingle, (asyncResult) => {
       if (asyncResult.status === Office.AsyncResultStatus.Failed) {
         console.log(asyncResult.error.message);
         return;
       }
 
-      console.log("Event handler added.");
+      console.log("ItemChanged Event handler added.");
+    });
+
+    Office.context.mailbox.addHandlerAsync(Office.EventType.SelectedItemsChanged, runMulti, (asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+        console.log(asyncResult.error.message);
+        return;
+      }
+
+      console.log("SelectedItemsChanged Event handler added.");
     });
   }
+
+  console.log("Initial item", Office.context.mailbox?.item);
 });
 
-export async function run() {
+export function runSingle() {
+  console.log("Single event");
+
+  // Clear list of previously selected messages, if any.
+  const list = document.getElementById("selected-items");
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  console.log("Selected item", Office.context.mailbox?.item);
+
+  const listItem = document.createElement("li");
+  listItem.textContent = Office.context.mailbox?.item?.subject ?? "[NO SELECTION]";
+  list.appendChild(listItem);
+
+  console.log("Closing...");
+  //Office.context.ui.closeContainer();
+}
+
+export async function runMulti() {
   // Clear list of previously selected messages, if any.
   const list = document.getElementById("selected-items");
   while (list.firstChild) {
@@ -33,14 +67,18 @@ export async function run() {
   // Retrieve the subject line of the selected messages and log it to a list in the task pane.
   //(Office.context.mailbox as any).initialData = { permissionLevel: 3 };
 
-  Office.context.mailbox.getSelectedItemsAsync(asyncResult => {
+  Office.context.mailbox.getSelectedItemsAsync((asyncResult) => {
+    console.log("Multi event");
+
     if (asyncResult.status === Office.AsyncResultStatus.Failed) {
       console.log(asyncResult.error.message);
       console.log(asyncResult.error);
       return;
     }
 
-    asyncResult.value.forEach(item => {
+    console.log("Selected items", asyncResult.value);
+
+    asyncResult.value.forEach((item) => {
       const listItem = document.createElement("li");
       listItem.textContent = item.subject;
       list.appendChild(listItem);
